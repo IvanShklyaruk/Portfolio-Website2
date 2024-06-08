8908efee6d6a9ee88d084592ce9e9fae4ef15ea3
IvanShklyaruk <shklyaruk.ivan@gmail.com>
2024-06-08 16:28:43 -0400

    Starter pack

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./components/Experience";
 
 function App() {
-  return <div> Hello World! </div>;
+  return (
+    <>
+      <Canvas camera={{ position: [0, 0.5, 5], fov: 42 }}>
+        <color attach="background" args={["#f5f3ee"]} />
+        <fog attach="fog" args={["#f5f3ee", 10, 50]} />
+        <group position-y={-1}>
+          <Experience />
+        </group>
+      </Canvas>
+    </>
+  );
 }
 
 export default App;
