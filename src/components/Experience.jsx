88c847c232b0dbe13a76ee3afedf06bf368c5c9d
IvanShklyaruk <shklyaruk.ivan@gmail.com>
2024-06-10 16:09:43 -0400

    Movement added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,44 @@
 import { Environment } from "@react-three/drei";
+import { useRef } from "react";
+import { Avatar } from "./Avatar";
+import { SectionTitle } from "./SectionTitle";
+import { useScroll } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+
+const SECTIONS_DISTANCE = 10;
 
 export const Experience = () => {
+  const sceneContainer = useRef();
+  const scrollData = useScroll();
+
+  useFrame(() => {
+    sceneContainer.current.position.z =
+      -scrollData.offset * SECTIONS_DISTANCE * (scrollData.pages - 1);
+  });
+
   return (
     <>
       <Environment preset="sunset" />
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial color="white" />
-      </mesh>
+      <Avatar />
+
+      <group ref={sceneContainer}>
+        {/* HOME */}
+        <group>
+          <SectionTitle position-x={0.5}>HOME</SectionTitle>
+        </group>
+        {/* SKILLS */}
+        <group position-z={SECTIONS_DISTANCE}>
+          <SectionTitle position-x={0.5}>SKILLS</SectionTitle>
+        </group>
+        {/* PROJECTS */}
+        <group position-z={2 * SECTIONS_DISTANCE}>
+          <SectionTitle position-x={0.5}>PROJECTS</SectionTitle>
+        </group>
+        {/* CONTACT */}
+        <group position-z={3 * SECTIONS_DISTANCE}>
+          <SectionTitle position-x={0.5}>CONTACT</SectionTitle>
+        </group>
+      </group>
     </>
   );
 };
