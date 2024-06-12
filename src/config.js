569a5c2cb4522b3ade0e4df891f06892f6f057b9
IvanShklyaruk <shklyaruk.ivan@gmail.com>
2024-06-11 20:56:28 -0400

    base works

--- src/config.js
+++ src/config.js
@@ -1,3 +1,80 @@
 export const config = {
+  title: "Software Developer",
   sections: ["home", "skills", "projects", "contact"],
+  home: {
+    title: "Ivan",
+    subtitle: "Shklyaruk",
+  },
+  skills: [
+    {
+      name: "React",
+      icon: "icons/react-native.png",
+      level: 80,
+    },
+    {
+      name: "JavaScript",
+      icon: "icons/javascript.png",
+      level: 80,
+    },
+    {
+      name: "Three.js",
+      icon: "icons/threejs.png",
+      level: 70,
+    },
+    {
+      name: "Blender",
+      icon: "icons/blender-3d.png",
+      level: 50,
+    },
+    {
+      name: "French",
+      icon: "icons/france.png",
+      level: 100,
+    },
+    {
+      name: "English",
+      icon: "icons/usa.png",
+      level: 90,
+    },
+    {
+      name: "Japanese",
+      icon: "icons/japan.png",
+      level: 30,
+    },
+  ],
+  projects: [
+    {
+      name: "???",
+      description: "???",
+      image: "projects/placeholder.png",
+      link: "",
+    },
+    {
+      name: "???",
+      description: "???",
+      image: "projects/placeholder.png",
+      link: "",
+    },
+    {
+      name: "???",
+      description: "???",
+      image: "projects/placeholder.png",
+      link: "",
+    },
+    {
+      name: "???",
+      description: "???",
+      image: "projects/placeholder.png",
+      link: "",
+    },
+  ],
+  contact: {
+    name: "Ivan Shklyaruk",
+    address: "Toronto, Canada",
+    socials: {
+      linkedin: "https://www.linkedin.com/in/ishklyar/",
+      twitter: "https://twitter.com/",
+    },
+    mail: "ishklyar@uwaterloo.ca",
+  },
 };
