1fb466624b42672e5ca1e71678be0ce87720a2d4
IvanShklyaruk <shklyaruk.ivan@gmail.com>
2024-07-24 02:50:14 -0400

    Updated projects and links, cleaned up, added a loading screen prompt, changhed logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Scroll, ScrollControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { MotionConfig } from "framer-motion";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Experience } from "./components/Experience";
 import { Interface } from "./components/Interface";
 import { LoadingScreen } from "./components/LoadingScreen";
@@ -9,36 +9,48 @@ import { Menu } from "./components/Menu";
 import { config } from "./config";
 
 function App() {
+  const [isLoadingComplete, setIsLoadingComplete] = useState(false);
+
+  const handleLoadingComplete = () => {
+    setIsLoadingComplete(true);
+  };
+
   return (
     <>
-      <LoadingScreen />
-      <Canvas camera={{ position: [0, 0.5, 5], fov: 42 }}>
-        <color attach="background" args={["#f5f3ee"]} />
-        <fog attach="fog" args={["#f5f3ee", 10, 50]} />
-        <ScrollControls
-          pages={config.sections.length}
-          damping={0.1}
-          maxSpeed={0.2}
-        >
-          <group position-y={-1}>
-            <MotionConfig
-              transition={{
-                duration: 0.6,
-              }}
+      {!isLoadingComplete && (
+        <LoadingScreen onLoadingComplete={handleLoadingComplete} />
+      )}
+      {isLoadingComplete && (
+        <>
+          <Canvas camera={{ position: [0, 0.5, 5], fov: 42 }}>
+            <color attach="background" args={["#f5f3ee"]} />
+            <fog attach="fog" args={["#f5f3ee", 10, 50]} />
+            <ScrollControls
+              pages={config.sections.length}
+              damping={0.1}
+              maxSpeed={0.2}
             >
-              <Suspense>
-                <Experience />
-              </Suspense>
-            </MotionConfig>
-          </group>
-          <Scroll html>
-            <MotionConfig transition={{ duration: 1 }}>
-              <Interface />
-            </MotionConfig>
-          </Scroll>
-        </ScrollControls>
-      </Canvas>
-      <Menu />
+              <group position-y={-1}>
+                <MotionConfig
+                  transition={{
+                    duration: 0.6,
+                  }}
+                >
+                  <Suspense>
+                    <Experience />
+                  </Suspense>
+                </MotionConfig>
+              </group>
+              <Scroll html>
+                <MotionConfig transition={{ duration: 1 }}>
+                  <Interface />
+                </MotionConfig>
+              </Scroll>
+            </ScrollControls>
+          </Canvas>
+          <Menu />
+        </>
+      )}
     </>
   );
 }
--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,11 +1,33 @@
 import { useProgress } from "@react-three/drei";
+import { useState, useEffect } from "react";
 import { config } from "../config";
 
-export const LoadingScreen = () => {
-  const { progress, active } = useProgress();
+export const LoadingScreen = ({ onLoadingComplete }) => {
+  const { progress } = useProgress();
+  const [isPromptVisible, setIsPromptVisible] = useState(true);
+  const [isLoadingVisible, setIsLoadingVisible] = useState(true);
+
+  useEffect(() => {
+    if (progress === 100 && !isPromptVisible) {
+      setIsLoadingVisible(false);
+      onLoadingComplete();
+    }
+  }, [progress, isPromptVisible, onLoadingComplete]);
+
+  const handleOkClick = () => {
+    setIsPromptVisible(false);
+    if (progress === 100) {
+      setIsLoadingVisible(false);
+      onLoadingComplete();
+    }
+  };
 
   return (
-    <div className={`loading-screen ${active ? "" : "loading-screen--hidden"}`}>
+    <div
+      className={`loading-screen ${
+        isLoadingVisible ? "" : "loading-screen--hidden"
+      }`}
+    >
       <div className="loading-screen__container">
         <h1 className="loading-screen__title">{config.title}</h1>
         <div className="progress__container">
@@ -14,6 +36,17 @@ export const LoadingScreen = () => {
             style={{ width: `${progress}%` }}
           ></div>
         </div>
+        {isPromptVisible && (
+          <div className="prompt">
+            <p className="prompt__message">
+              If you experience lag, please consider using an alternative
+              browser to Chrome!
+            </p>
+            <button className="prompt__close" onClick={handleOkClick}>
+              OK
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
